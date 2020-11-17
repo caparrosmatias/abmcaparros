a6b18735808c70853b58ff437f1517eac03bbbd0
Matias Caparros <55497544+caparrosmatias@users.noreply.github.com>
2020-11-17 19:26:00 -0300

    abm universidad

---
+++ ABMCAPARROS/apps/gestion/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from ABMCAPARROS.apps.gestion.models import *
+
+# Register your models here.
+
+admin.site.register(Alumno)
+admin.site.register(Curso)
+admin.site.register(Matricula)
+
+
+
